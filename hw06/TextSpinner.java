159a02d3048fac1bad8876a2f3a0e2cf5191ae35
joh216 <joh216@lehigh.edu>
2016-03-08 22:01:22 +0000

    Updated TextSpinner

--- hw06/TextSpinner.java
+++ hw06/TextSpinner.java
@@ -8,22 +8,19 @@ Purpose is to practice writing and using loops
 public class TextSpinner{ 
     public static void main(String[] arg){
         
-        int number = 1; 
-        
-        while(number >= 1){ 
-            switch(number%4){
-                case 1: System.out.print("/"); 
-                break; 
-                case 2: System.out.print("-"); 
-                break; 
-                case 3: System.out.print("\\"); 
-                break; 
-                case 0: System.out.print("|"); 
-                break; 
-            }
-            number++;
+        while(true){ 
+            
+            System.out.print("/"); 
+                System.out.print("\b"); 
+                System.out.print("-"); 
+               System.out.print("\b"); 
+                System.out.print("\\"); 
+                 System.out.print("\b"); 
+               System.out.print("|"); 
+                 System.out.print("\b"); 
             }
             
-            System.out.println();
+            
+        
     }
 }
